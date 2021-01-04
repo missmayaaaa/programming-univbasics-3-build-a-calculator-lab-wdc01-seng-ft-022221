ff03c21d39ac040fc5f238befae639744fff7bac
Maya Grant <maya.grant@utexas.edu>
2021-01-04 16:34:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,16 @@
 def addition(num1, num2)
   add = num1 + num2
   return add
-
 end
 
 def subtraction(num1, num2)
   subtract = num1 - num2
   return subtract
-
 end
 
 def division(num1, num2)
   div = num1 / num2
   return = div
-
 end
 
 def multiplication(num1, num2)
@@ -24,7 +21,6 @@ end
 def modulo(num1, num2)
   mod = num1 % num2
   return = mod
-
 end
 
 def square_root(num)
